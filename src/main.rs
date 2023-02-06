db485cb171455ea5e50ecbebeb46e95958f05aaa
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-05 18:40:23 -0600

    creating main loop

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use crate::templates::User::User;
 
 
 fn main(){
-    let usr: User = User::new("Oliver".to_string(), "daniela11".to_string(), 
-                              "olstertecn597@gmail.com".to_string(), "outlook".to_string());
-    
-    usr._print();
+    // let usr: User = User::new("Oliver".to_string(), "daniela11".to_string(), 
+                              // "olstertecn597@gmail.com".to_string(), "outlook".to_string());
+    // usr._print();
+    let _users: Vec<User> = Vec::new();
+    let _run: bool = true;
+    while _run {
+
+    }
 
 }
