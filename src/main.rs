e61b8bbe938d1125c1375004ee8b992d92382d83
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-05 18:55:50 -0600

    adding menu and 3 basic options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod templates;
-use crate::templates::User::User;
+use crate::{templates::User::User, utils::{readline, read_number, clear, pause}};
+mod utils;
 
 
 fn main(){
@@ -9,7 +10,36 @@ fn main(){
     let _users: Vec<User> = Vec::new();
     let _run: bool = true;
     while _run {
+        clear();
+        println!("1- Add User");
+        println!("2- List Users");
+        println!("3- Exit");
+        println!("Insert a option: ");
+        let option: i32 = read_number();
 
+        if option == 1{
+            clear();
+            let name: String = readline("Ingrese un nombre: ".to_string());
+            let pass: String = readline("Ingrese una contraseña: ".to_string());
+            let email: String = readline("Ingrese un correo: ".to_string());
+            let desc: String = readline("Ingrese una descripcion de la cuenta: ".to_string());
+            let new_user: User = User::new(name, pass, email, desc);
+            _users.push(new_user);
+            pause();
+        }
+        if option == 2{
+            clear();
+            for usr in _users{
+                usr._print();
+            }
+            pause();
+        }
+        
+        if option == 3{
+            clear();
+            _run = false;
+            pause();
+        }
     }
 
 }
