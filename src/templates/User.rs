82b70c5d9545890d12d6bb7c767f2d0b04402d37
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-05 18:21:39 -0600

    adding implementation

--- src/templates/User.rs
+++ src/templates/User.rs
@@ -1,6 +1,18 @@
-pub(crate)struct User{
+pub struct User{
     username: String,
     password: String,
     email: String,
     description: String
+}
+
+
+impl User{
+    pub fn new(username: String, pass: String, email: String, desc: String)->Self{
+        User{
+            username: username,
+            password: pass,
+            email: email,
+            description: desc
+        }
+    }
 }
