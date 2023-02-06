95dcd3a7ba1cafd13dd3ff111e3b1b10af329dd5
Olstertecn11 <otznund@miumg.edu.gt>
2023-02-05 18:32:35 -0600

    printing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crate::templates::User::User;
 fn main(){
     let usr: User = User::new("Oliver".to_string(), "daniela11".to_string(), 
                               "olstertecn597@gmail.com".to_string(), "outlook".to_string());
-    println!("{}", usr.usrename);
     
+    usr._print();
 
 }
